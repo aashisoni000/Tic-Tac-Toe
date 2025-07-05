6ddd594d7eb4efb0cfb5f07b6ba24eea18fd6aa3
aashisoni000 <aashi2701soni@gmail.com>
2025-07-06 01:37:37 +0530

    game logic done, added winner section(incomplete)

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -36,5 +39,11 @@ const checkWinner = () => {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if (pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner", pos1Val);
+            }
+        }
     }
 }
